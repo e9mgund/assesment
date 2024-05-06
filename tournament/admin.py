78407ed77828027cae4c89d8578b5f6a666e03a4
e9mgund <mayur.gund@enine.school>
2024-05-06 12:23:28 +0530

    Task_15 commit

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Team
+from .models import Team , Match , TeamMatch
 # Register your models here.
 admin.site.register(Team)
+admin.site.register(Match)
+admin.site.register(TeamMatch)
