e47c11001cee8746dc1dab9c8cb9ffea87079db2
e9mgund <mayur.gund@enine.school>
2024-05-06 10:40:49 +0530

    task_06 commit

--- project/settings/settings.py
+++ project/settings/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'tournament.apps.TournamentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
