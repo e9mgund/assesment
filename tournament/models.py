efbb51ad0a22096d750485a06169bc53fd2e77f1
e9mgund <mayur.gund@enine.school>
2024-05-06 11:21:53 +0530

    Task_12 , Task_13 , Task_14 commit

--- tournament/models.py
+++ tournament/models.py
@@ -3,3 +3,25 @@ from django.db import models
 # Create your models here.
 class Team(models.Model):
     country = models.CharField(max_length=3, unique=True)
+
+    def matches_played(self) -> int:
+        pass
+
+    def matches_won(self) -> int:
+        pass
+
+    def matches_lost(self) -> int:
+        pass
+
+    def __str__(self):
+        return self.country
+
+class Match(models.Model):
+    date = models.DateField()
+
+class TeamMatch(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team_matches')
+    match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='team_matches')
+    batting_score = models.IntegerField()
+    batting_wickets = models.IntegerField()
+    batting_overs = models.IntegerField()
